ec5f7d18d290e6f6a58dfc9f46ac5bd81e0df84e
andrea1632 <andrea.bellucci10@libero.it>
2022-05-23 17:17:24 +0200

    main nav bar

---
+++ resources/views/partials/mainNav.blade.php
@@ -0,0 +1,24 @@
+<nav class="mainNav">
+    <ul>
+        <li>
+            <img src="{{asset('images/buy-comics-digital-comics.png')}}" alt="">
+            <span>DIGITAL COMICS</span>
+        </li>
+        <li>
+            <img src="{{asset('images/buy-comics-merchandise.png')}}" alt="">
+            <span>DC MERCHANDISE</span>
+        </li>
+        <li>
+            <img src="{{asset('images/buy-comics-subscriptions.png')}}" alt="">
+            <span>SUBSCRIPTION</span>
+        </li>
+        <li>
+            <img src="{{asset('images/buy-comics-shop-locator.png')}}" alt="">
+            <span>SHOP LOCATOR</span>
+        </li>
+        <li>
+            <img src="{{asset('images/buy-dc-power-visa.svg')}}" alt="">
+            <span>DC POWER VISA</span>
+        </li>
+    </ul>
+</nav>
