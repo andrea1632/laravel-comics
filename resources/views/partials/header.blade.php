628fb84804519390ce1e88f8c1e3244367f4781a
andrea1632 <andrea.bellucci10@libero.it>
2022-05-23 15:50:14 +0200

    added header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,44 @@
+<header>
+    <div class="headerTop">
+        <div>
+            <span>DC POWER VISA</span>
+            <span>ADDITIONAL DC SITES</span>
+        </div>
+    </div>
+    <nav>
+        <img src="{{asset('images/dc-logo.png')}}" alt="">
+        <ul>
+            <li>
+                <a href="">characters</a>
+            </li>
+            <li>
+                <a href="">comics</a>
+            </li>
+            <li>
+                <a href="">movies</a>
+            </li>
+            <li>
+                <a href="">tv</a>
+            </li>
+            <li>
+                <a href="">games</a>
+            </li>
+            <li>
+                <a href="">collectibles</a>
+            </li>
+            <li>
+                <a href="">videos</a>
+            </li>
+            <li>
+                <a href="">fans</a>
+            </li>
+            <li>
+                <a href="">news</a>
+            </li>
+            <li>
+                <a href="">shop</a>
+            </li>
+        </ul>
+        <input type="text" placeholder="Search">
+    </nav>
+</header>
